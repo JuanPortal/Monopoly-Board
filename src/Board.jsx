ec22d82d126f51241de1ae4c892a9726bc48e646
Portal <juanportalg@gmail.com>
2023-05-20 20:21:40 -0700

    testing on a Redmi Note 11

--- src/Board.jsx
+++ src/Board.jsx
@@ -5,8 +5,8 @@ import { data } from './data'
 export const Board = () => {    
     return (
         <main>
-            {/* <Row location="north" data={data.slice(0, 10)} /> */}
-            {/* <Row location="east" data={data.slice(10, 20)} /> */}
+            <Row location="north" data={data.slice(0, 10)} />
+            <Row location="east" data={data.slice(10, 20)} />
             <Row location="south" data={data.slice(20, 30)} />
             <Row location="west" data={data.slice(30, 40)} />
         </main>
