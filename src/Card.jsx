405776f13fa43227322b2975f07c039fbe8c31cf
Portal <juanportalg@gmail.com>
2023-05-20 21:39:11 -0700

    icons added

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faToolbox, faMoneyCheckDollar, faTrain, faQuestion, faLightbulb, faFaucetDrip, faHandcuffs } from '@fortawesome/free-solid-svg-icons'
+library.add(fas, faToolbox, faMoneyCheckDollar, faTrain, faQuestion, faLightbulb, faFaucetDrip, faHandcuffs)
 
-export const Card = ({ type, color, name, tag }) => {
+export const Card = ({ type, color, icon, name, tag }) => {
   return (
     <div className={`card ${type}`}>
-        <div className="color"></div>
+        <div className="color">
+          {
+            icon ? <FontAwesomeIcon icon={`fa-solid fa-${icon}`} /> : ""
+          }
+        </div>
         <div className="name">{name}</div>
         <div className="tag">{tag}</div>
-    </div>
+    </div>    
   )
 }
--- src/Row.jsx
+++ src/Row.jsx
@@ -6,7 +6,7 @@ export const Row = ({ location, data }) => {
         <section className={`row ${location}`}>
             {data.map(data => {
                 return (
-                    <Card key={data[0]} position={data[0]} type={data[1]} color={data[2]} name={data[3]} tag={data[4]} value={data[5]} />
+                    <Card key={data[0]} position={data[0]} type={data[1]} color={data[2]} icon={data[3]} name={data[4]} tag={data[5]} value={data[6]} />
                 );
             })}
         </section>
