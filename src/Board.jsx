a12348e96a33caf146916cb44d375e7482085e3b
Portal <juanportalg@gmail.com>
2023-05-20 19:39:44 -0700

    data array creating and imported to the Component

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Row } from './Row'
+import { data } from './data'
 
-export const Board = () => {
-  return (
-    <main>
-        <Row location="west" />
-    </main>
+export const Board = () => {    
+    return (
+        <main>
+            {/* <Row location="north" data={data.slice(0, 10)} /> */}
+            {/* <Row location="east" data={data.slice(10, 20)} /> */}
+            <Row location="south" data={data.slice(20, 30)} />
+            <Row location="west" data={data.slice(30, 40)} />
+        </main>
   )
 }
